2a32d05b12ec6da35d2960bf2418698d424f5804
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-24 16:50:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Creating the student object with a name property
+let student = {
+  name: 'Aryan'
+};
+
+// Adding the getKeys method to Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Getting the keys of the student object
+console.log(student.getKeys());  // Output: ['name']
+
